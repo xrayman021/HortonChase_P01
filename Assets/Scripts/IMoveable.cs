8b4fa8d2f9a813c47e1e81f335f87d3aa3dfc5bf
Chase's Hub <chaseghorton99@gmail.com>
2021-09-09 15:16:40 -0500

    Created IMoveable script

---
+++ Assets/Scripts/IMoveable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Examples.Strategy
+{
+    public interface IMoveable
+    {
+        void Move();
+    }
+}
